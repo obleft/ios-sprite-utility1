51b94d6e5b72595828a22e52257ecfbd120e7550
Ben Hakes <bhhakes@gmail.com>
2018-11-05 15:19:16 -0600

    replace move and turn with zoom on move

--- FIrstSprites/FIrstSprites/CustomScene.swift
+++ FIrstSprites/FIrstSprites/CustomScene.swift
@@ -20,12 +20,11 @@ class CustomScene: SKScene {
         let position = touch.location(in: self)
         
         // Create move action
-        let actionDuration = 1.0
-        let moveAction = SKAction.move(to: position, duration: actionDuration)
-        let turnAction = SKAction.rotate(byAngle: CGFloat.pi * 2, duration: actionDuration)
-        let groupAction = SKAction.group([moveAction, turnAction])
+        let zoomAction = SKAction.scale(by: 1.3, duration: 0.3)
+        let unzoomAction = SKAction.scale(to: 1.0, duration: 0.1)
+        let sequenceAction = SKAction.sequence([zoomAction, moveAction, unzoomAction])
+        crab.run(sequenceAction)
         
-        crab.run(groupAction)
         
         
         // Run move action
